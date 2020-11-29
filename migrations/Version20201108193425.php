434557a0cc5c61f2d793a312e636c5da6e9253d7
AnastasiyaHahanova <anastasiya.hahanova@mail.ru>
2020-11-29 03:38:45 +0300

    commit

--- migrations/Version20201108193425.php
+++ migrations/Version20201108193425.php
@@ -12,19 +12,19 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201108193425 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE table_value (id INT AUTO_INCREMENT NOT NULL, table_id_id INT NOT NULL, row INT NOT NULL NOT NULL, `column` INT NOT NULL, value DOUBLE PRECISION NOT NULL, INDEX IDX_118B954C73B8532F (table_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE table_value ADD CONSTRAINT FK_118B954C73B8532F FOREIGN KEY (table_id_id) REFERENCES table_value (id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE table_value DROP FOREIGN KEY FK_118B954C73B8532F');
